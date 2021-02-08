d12b9b291b7151354343b0adb87262560686d5c0
Вадим кохановский <koxanovskij86@mail.ru>
2021-02-09 00:17:29 +0300

    00.01 9 февраля 2021 г.

--- src/main/java/Homework6/Animal.java
+++ src/main/java/Homework6/Animal.java
@@ -2,7 +2,6 @@ package Homework6;
 
 public class Animal {
     String name;
-    public int maxDistanceTraveled;
 
     public void run(int distance) {
         System.out.println(name + " пробежал " + distance + " метров");
