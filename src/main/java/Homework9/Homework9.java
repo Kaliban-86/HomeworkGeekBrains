e6e8e854b8b3138d50ef0886b15e0368b690489d
Вадим кохановский <koxanovskij86@mail.ru>
2021-02-21 11:51:21 +0300

    Добавлено исключение по размеру массива, но некраисово составлено условие.

--- src/main/java/Homework9/Homework9.java
+++ src/main/java/Homework9/Homework9.java
@@ -8,40 +8,33 @@ package Homework9;
 //         и вывести результат расчета.
 
 public class Homework9 {
+    public static final int ARR_X = 4;
+    public static final int ARR_Y = 4;
+
+    public static void main(String[] args) throws MyArraySizeException {
+
+        String[][] str = {
+                {"1", "1", "1", "1"},
+                {"1", "1", "1", "1"},
+                {"1", "1", "1", "1"},
+                {"1", "1", "1", "1"},
 
 
-    public static void main(String[] args) {
+        };
 
-        String[][] str = new String[4][4];
-
-        str[0][0] = "1";
-        str[0][1] = "0";
-        str[0][2] = "1";
-        str[0][3] = "1";
-
-        str[1][0] = "1";
-        str[1][1] = "1";
-        str[1][2] = "8";
-        str[1][3] = "1";
-
-        str[2][0] = "1";
-        str[2][1] = "1";
-        str[2][2] = "1";
-        str[2][3] = "1";
-
-        str[3][0] = "1";
-        str[3][1] = "7";
-        str[3][2] = "1";
-        str[3][3] = "1";
 
         System.out.println(arrTransformation(str));
     }
 
-    public static int arrTransformation(String[][] arr) {
+    public static int arrTransformation(String[][] arr) throws MyArraySizeException {
+        if (arr.length != ARR_Y || arr[0].length != ARR_X || arr[1].length != ARR_X || arr[2].length != ARR_X || arr[3].length != ARR_X) {
+            throw new MyArraySizeException();
+        }
+
         int sum = 0;
-        for (String[] strings : arr) {
-            for (int j = 0; j < arr.length; j++) {
-                sum = sum + Integer.parseInt(strings[j]);
+        for (int i = 0; i < ARR_Y; i++) {
+            for (int j = 0; j < ARR_X; j++) {
+                sum = sum + Integer.parseInt(arr[i][j]);
 
             }
         }
