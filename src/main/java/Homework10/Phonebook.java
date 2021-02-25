8528ab9ac85c49dbf2bb075f7ddf548489a0b14b
Вадим кохановский <koxanovskij86@mail.ru>
2021-02-25 23:48:48 +0300

    Домашняя работа №10 для сдачи.

--- src/main/java/Homework10/Phonebook.java
+++ src/main/java/Homework10/Phonebook.java
@@ -1,18 +1,24 @@
 package Homework10;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Phonebook {
+    static List<String> names = new ArrayList<>();
+    static List<Integer> phoneNumbers = new ArrayList<>();
 
-    Map<String, Integer> baseOfPhoneNumbers = new HashMap<>();
-
-    public  void add(String name, int phoneNumber){
-        baseOfPhoneNumbers.put(name,phoneNumber);
+    public void add(String name, int phoneNumber) {
+        names.add(name);
+        phoneNumbers.add(phoneNumber);
     }
 
-    public void get(String name){
-        System.out.println(baseOfPhoneNumbers.get(name)) ;
+    public void get(String name) {
+        System.out.print(name.toLowerCase() + " - ");
+        for (int i = 0; i < names.size(); i++) {
+            if (names.get(i).equalsIgnoreCase(name)) {
+                System.out.print(phoneNumbers.get(i) + " # ");
+            }
+        }
+        System.out.println();
     }
-
 }
