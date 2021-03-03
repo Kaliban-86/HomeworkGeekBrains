1927e13733cc2b33b505b9296188613c15344411
Вадим кохановский <koxanovskij86@mail.ru>
2021-03-04 00:35:30 +0300

    Осталось только разбить на потоки

--- src/main/java/Homework12/Homework12.java
+++ src/main/java/Homework12/Homework12.java
@@ -20,38 +20,44 @@ package Homework12;
 //        -  Второй разбивает массив на два массива, в двух потоках высчитывает новые значения и потом склеивает
 //          эти массивы обратно в один.
 //
-//        Пример деления одного массива на два:
-//        System.arraycopy(arr, 0, a1, 0, h);
-//        System.arraycopy(arr, h, a2, 0, h).
+//        + Пример деления одного массива на два:
+//          System.arraycopy(arr, 0, a1, 0, h);
+//          System.arraycopy(arr, h, a2, 0, h).
 //
-//        Пример обратной склейки:
-//        System.arraycopy(a1, 0, arr, 0, h);
-//        System.arraycopy(a2, 0, arr, h, h).
+//        + Пример обратной склейки:
+//          System.arraycopy(a1, 0, arr, 0, h);
+//          System.arraycopy(a2, 0, arr, h, h).
 //
-//        Примечание:
-//        System.arraycopy() — копирует данные из одного массива в другой:
-//        System.arraycopy(массив-источник, откуда начинаем брать данные из массива-источника, массив-назначение,
-//        откуда начинаем записывать данные в массив-назначение, сколько ячеек копируем)
-//        По замерам времени:
+//          Примечание:
+//          System.arraycopy() — копирует данные из одного массива в другой:
+//          System.arraycopy(массив-источник, откуда начинаем брать данные из массива-источника, массив-назначение,
+//          откуда начинаем записывать данные в массив-назначение, сколько ячеек копируем)
+//          По замерам времени:
 //        + Для первого метода надо считать время только на цикл расчета:
 //          for (int i = 0; i < size; i++) {
 //          arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
 //          }
-//        - Для второго метода замеряете время разбивки массива на 2, просчета каждого из двух массивов и склейки.
+//        + Для второго метода замеряете время разбивки массива на 2, просчета каждого из двух массивов и склейки.
 
 import java.util.Arrays;
 
 public class Homework12 {
     public static void main(String[] args) {
+
         methodOne();
         methodTwo();
+
+
     }
 
     public static void methodOne() {
         final int SIZE = 1000000;
         float[] arr = new float[SIZE];
+
         Arrays.fill(arr, 1);
+
         long a = System.currentTimeMillis();
+
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
@@ -59,7 +65,7 @@ public class Homework12 {
     }
 
     public static void methodTwo() {
-        final int SIZE = 10;
+        final int SIZE = 1000000;
         final int HALF = SIZE / 2;
         float[] arr = new float[SIZE];
         float[] arrHalf1 = new float[HALF];
@@ -72,10 +78,16 @@ public class Homework12 {
         System.arraycopy(arr, 0, arrHalf1, 0, HALF);
         System.arraycopy(arr, HALF, arrHalf2, 0, HALF);
 
-
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        for (int i = 0; i < arrHalf1.length; i++) {
+            arrHalf1[i] = (float) (arrHalf1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
+        for (int i = 0; i < arrHalf2.length; i++) {
+            arrHalf2[i] = (float) (arrHalf2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+
+        System.arraycopy(arrHalf1, 0, arr, 0, HALF);
+        System.arraycopy(arrHalf2, 0, arr, HALF, HALF);
+
         System.out.println(System.currentTimeMillis() - a);
     }
 }
--- src/main/java/Homework12/Test.java
+++ src/main/java/Homework12/Test.java
@@ -12,18 +12,28 @@ public class Test {
             arr[i] = i;
         }
 
-        System.arraycopy(arr, 0, new float[HALF], 0, HALF);
-        System.arraycopy(arr, HALF, new float[HALF], 0, HALF);
+        System.arraycopy(arr, 0, a1, 0, HALF);
+        System.arraycopy(arr, HALF, a2, 0, HALF);
 
         for (float v : a1) {
             System.out.print(v + " ");
 
         }
+        System.out.println();
         System.out.println("второй массив");
-
 
         for (int i = 0; i < a1.length; i++) {
             System.out.print(a2[i]+ " ");
+        }
+        System.arraycopy(a1, 0, arr, 0, HALF);
+        System.arraycopy(a2, 0, arr, HALF, HALF);
+
+        System.out.println();
+        System.out.println("третий массив");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+
         }
     }
 }
