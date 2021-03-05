7053d65f5987f832bee2335b684a3ac67fb0843b
Вадим кохановский <koxanovskij86@mail.ru>
2021-03-05 22:16:58 +0300

    Готовое ДЗ №12

--- src/main/java/Homework12/Homework12.java
+++ src/main/java/Homework12/Homework12.java
@@ -17,7 +17,7 @@ package Homework12;
 //
 //        Отличие первого метода от второго:
 //        + Первый просто бежит по массиву и вычисляет значения.
-//        -  Второй разбивает массив на два массива, в двух потоках высчитывает новые значения и потом склеивает
+//        +  Второй разбивает массив на два массива, в двух потоках высчитывает новые значения и потом склеивает
 //          эти массивы обратно в один.
 //
 //        + Пример деления одного массива на два:
@@ -46,14 +46,11 @@ public class Homework12 {
 
         methodOne();
         methodTwo();
-
-
     }
 
     public static void methodOne() {
         final int SIZE = 1000000;
         float[] arr = new float[SIZE];
-
         Arrays.fill(arr, 1);
 
         long a = System.currentTimeMillis();
@@ -61,33 +58,47 @@ public class Homework12 {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
-        System.out.println(System.currentTimeMillis() - a);
+        System.out.println("Время работы 1 метода: " + (System.currentTimeMillis() - a));
     }
 
     public static void methodTwo() {
         final int SIZE = 1000000;
         final int HALF = SIZE / 2;
         float[] arr = new float[SIZE];
-        float[] arrHalf1 = new float[HALF];
-        float[] arrHalf2 = new float[HALF];
-
+        float[] arr1 = new float[HALF];
+        float[] arr2 = new float[HALF];
         Arrays.fill(arr, 1);
 
         long a = System.currentTimeMillis();
 
-        System.arraycopy(arr, 0, arrHalf1, 0, HALF);
-        System.arraycopy(arr, HALF, arrHalf2, 0, HALF);
+        System.arraycopy(arr, 0, arr1, 0, HALF);
+        System.arraycopy(arr, HALF, arr2, 0, HALF);
 
-        for (int i = 0; i < arrHalf1.length; i++) {
-            arrHalf1[i] = (float) (arrHalf1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < arr1.length; i++) {
+                arr1[i] = (float) (arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < arr2.length; i++) {
+                arr2[i] = (float) (arr2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        for (int i = 0; i < arrHalf2.length; i++) {
-            arrHalf2[i] = (float) (arrHalf2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        }
 
-        System.arraycopy(arrHalf1, 0, arr, 0, HALF);
-        System.arraycopy(arrHalf2, 0, arr, HALF, HALF);
+        System.arraycopy(arr1, 0, arr, 0, HALF);
+        System.arraycopy(arr2, 0, arr, HALF, HALF);
+        System.out.println("Время работы 2 метода: " + (System.currentTimeMillis() - a));
 
-        System.out.println(System.currentTimeMillis() - a);
     }
 }
